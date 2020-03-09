d2ce5bbda0037e97cd1f4da8afd262f02965aa1d
JT <mccrone@umich.edu>
2020-03-09 16:56:10 +0000

    coalescent nodes

--- src/App.js
+++ src/App.js
@@ -31,16 +31,16 @@ function App() {
   const width=1000,height=600,margins={top:50,right:50,bottom:75,left:20};
 
   if(tree!==null){
-      console.log(tree)
       const scheme = schemeTableau10.concat(schemeSet3);
       const colorScale = scaleOrdinal().domain(tree.annotationTypes.location.values).range(scheme);
       const timeScale = scaleTime().domain(tree.annotationTypes.date.extent).range([0,(width-margins.left-margins.right)]);
-        console.log(timeScale.domain())
       return (
           <svg width={width} height={height}>
               <FigTree width={width} height={height} margins={margins} tree={tree}>
                   <Nodes.Circle filter={(v=>v.node.children===null)} attrs={{r:4,fill:v=>colorScale(v.node.annotations.location)}} hoveredAttrs={{r:9}}/>
-                  <Branches attrs={{stroke:e=>e.v1.node.annotations.location? colorScale(e.v1.node.annotations.location):"grey"}}/>
+                  <Branches.Coalescent filter={(e=>e.v0.node.children.length>2)} attrs={{strokeWidth:2, stroke:e=>e.v1.node.annotations.location? colorScale(e.v1.node.annotations.location):"grey"}}/>
+                  <Nodes.Coalescent filter={(v=>v.node.children && v.node.children.length>2)} attrs={{fill:v=>(v.node.annotations.location?colorScale(v.node.annotations.location):"grey")}}/>
+                  <Branches.Rectangular filter={(e=>e.v0.node.children.length<=2)} attrs={{strokeWidth:2, stroke:e=>e.v1.node.annotations.location? colorScale(e.v1.node.annotations.location):"grey"}}/>
                   <Axis direction={"horizontal"} scale={timeScale} gap={10}
                         ticks={{number: 5, format: timeFormat("%y-%m-%d"), padding: 20, style: {}, length: 6}}>
                       <AxisBars lift={5}/>
@@ -49,7 +49,7 @@ function App() {
           </svg>
       )
   }else{
-      return <p>Loading</p>
+      return <p>Loading tree</p>
   }
 
 
