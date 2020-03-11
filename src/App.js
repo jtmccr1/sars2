913f2c31b64b11f9da7080a6d222df8a3f8fdb6e
JT <mccrone@umich.edu>
2020-03-11 16:07:17 +0000

    first pass at map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {parseNexus,FigTree,Nodes,collapseUnsupportedNodes,
-    orderByNodeDensity,Branches,annotateNode,Axis,Legend,NodeBackgrounds,
+    orderByNodeDensity,Branches,annotateNode,Axis,Legend,NodeBackgrounds,Map,Features,
 AxisBars} from "figtreejs-react"
 import{tsv} from "d3-fetch";
 import {schemeTableau10,schemeSet3} from "d3-scale-chromatic";
 import {scaleOrdinal, scaleTime} from "d3-scale";
 import {timeFormat} from "d3-time-format";
+import {geoEqualEarth} from "d3-geo";
+import {feature} from "topojson-client";
 
 const processTree=tree=> {
     return collapseUnsupportedNodes(orderByNodeDensity(tree, false), node => node.annotations.posterior < 0.5);
 };
 function App() {
-
   const [tree,setTree]=useState(null);
+  const [geographies,setGeographies] = useState(null);
   useEffect(()=>{
     fetch(process.env.PUBLIC_URL+"/data/exp.MCC.txt")
         .then(res=> res.text())
@@ -28,31 +30,64 @@ function App() {
         })
   },[]);
 
+
+  useEffect(()=>{
+      fetch(process.env.PUBLIC_URL+"/data/world-110m.json")
+          .then(response => {
+          if (response.status !== 200) {
+              console.log(`There was a problem: ${response.status}`);
+              return
+          }
+          response.json().then(worlddata => {
+              setGeographies(feature(worlddata, worlddata.objects.countries).features)
+          })
+      })
+  },[]);
+
+
   const width=1000,height=600,margins={top:50,right:50,bottom:75,left:20};
 
-  if(tree!==null){
+  if(tree!==null&&geographies!==null){
       const scheme = schemeTableau10.concat(schemeSet3);
       const colorScale = scaleOrdinal().domain(tree.annotationTypes.location.values).range(scheme);
       const timeScale = scaleTime().domain(tree.annotationTypes.date.extent).range([0,(width-margins.left-margins.right)]);
-      return (
-          <svg width={width} height={height}>
-              <FigTree width={width} height={height} margins={margins} tree={tree}>
-                  <Nodes.Coalescent filter={(v=>v.node.children && v.node.children.length>2)} attrs={{fill:v=>(v.node.annotations.location?colorScale(v.node.annotations.location):"grey")}}/>
-                  <NodeBackgrounds.Circle filter={(v=>v.node.children===null)} attrs={{r:5,fill:"black"}}/>
 
-                  <Nodes.Circle filter={(v=>v.node.children===null)} attrs={{r:4,fill:v=>colorScale(v.node.annotations.location),strokeWidth:0,stroke:"black"}} hoveredAttrs={{r:9,strokeWidth:1}}/>
-                  <Branches.Coalescent filter={(e=>e.v0.node.children.length>2)} attrs={{strokeWidth:2, stroke:e=>e.v1.node.annotations.location? colorScale(e.v1.node.annotations.location):"grey"}}/>
-                  <Branches.Rectangular filter={(e=>e.v0.node.children.length<=2)} attrs={{strokeWidth:2, stroke:e=>e.v1.node.annotations.location? colorScale(e.v1.node.annotations.location):"grey"}}/>
-                  <Axis direction={"horizontal"} scale={timeScale} gap={10}
-                        ticks={{number: 5, format: timeFormat("%y-%m-%d"), padding: 20, style: {}, length: 6}}>
-                      <AxisBars lift={5}/>
-                  </Axis>
-                  <Legend.Discrete height={300} columns={1} width={100} pos={{x:900,y:50}} scale={colorScale}/>
-              </FigTree>
-          </svg>
+      const projection = geoEqualEarth()
+          .translate([ width / 2, height / 2 ]);
+
+      return (
+          <>
+              <svg width={width} height={height}>
+                  <FigTree width={width} height={height} margins={margins} tree={tree}>
+                      <Nodes.Coalescent filter={(v=>v.node.children && v.node.children.length>2)} attrs={{fill:v=>(v.node.annotations.location?colorScale(v.node.annotations.location):"grey")}}/>
+                      <NodeBackgrounds.Circle filter={(v=>v.node.children===null)} attrs={{r:5,fill:"black"}}/>
+
+                      <Nodes.Circle filter={(v=>v.node.children===null)} attrs={{r:4,fill:v=>colorScale(v.node.annotations.location),strokeWidth:0,stroke:"black"}} hoveredAttrs={{r:9,strokeWidth:1}}>
+                          {/*<ToolTip onHover>*/}
+                          {/*    <p>Hello!</p>*/}
+                          {/*</ToolTip>*/}
+                      </Nodes.Circle>
+                      <Branches.Coalescent filter={(e=>e.v0.node.children.length>2)} attrs={{strokeWidth:2, stroke:e=>e.v1.node.annotations.location? colorScale(e.v1.node.annotations.location):"grey"}}/>
+                      <Branches.Rectangular filter={(e=>e.v0.node.children.length<=2)} attrs={{strokeWidth:2, stroke:e=>e.v1.node.annotations.location? colorScale(e.v1.node.annotations.location):"grey"}}/>
+                      <Axis direction={"horizontal"} scale={timeScale} gap={10}
+                            ticks={{number: 5, format: timeFormat("%y-%m-%d"), padding: 20, style: {}, length: 6}}>
+                          <AxisBars lift={5}/>
+                      </Axis>
+                      <Legend.Discrete height={300} columns={1} width={100} pos={{x:900,y:50}} scale={colorScale}/>
+                  </FigTree>
+              </svg>
+
+
+
+              <svg width={width} height={height}>
+                  <Map projection = {projection}>
+                    <Features geographies={geographies} attrs={{stroke:"black"}}/>
+                  </Map>
+              </svg>
+          </>
       )
   }else{
-      return <p>Loading tree</p>
+      return <p>Loading data</p>
   }
 
 
