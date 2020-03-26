c9c4cbc06adcaae7cc9a996cc1fe20d0e7309a63
jtmccr1 <mccronejt@gmail.com>
2020-03-26 11:26:02 +0000

    Updates

--- precache-manifest.69346ec732f582c94d4bd6bcdbf14997.js
+++ precache-manifest.69346ec732f582c94d4bd6bcdbf14997.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c444501aeab8974cec48c96bb434976",
+    "revision": "d13acf5fad3cbfc6fe419f26caae424a",
     "url": "/sars2/index.html"
   },
   {
-    "revision": "40ec4ed9dd8b9d485b03",
+    "revision": "06b747bbfa7f3a714983",
     "url": "/sars2/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "975e42734f2ae311b311",
-    "url": "/sars2/static/js/2.4ce635f2.chunk.js"
+    "revision": "2974497263ed05ad6f75",
+    "url": "/sars2/static/js/2.c165f321.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/sars2/static/js/2.4ce635f2.chunk.js.LICENSE.txt"
+    "url": "/sars2/static/js/2.c165f321.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "40ec4ed9dd8b9d485b03",
-    "url": "/sars2/static/js/main.7a5884dd.chunk.js"
+    "revision": "06b747bbfa7f3a714983",
+    "url": "/sars2/static/js/main.18e5a04e.chunk.js"
   },
   {
     "revision": "5253c5774dd9a9f2285c074f3e5129ab",
-    "url": "/sars2/static/js/main.7a5884dd.chunk.js.LICENSE.txt"
+    "url": "/sars2/static/js/main.18e5a04e.chunk.js.LICENSE.txt"
   },
   {
     "revision": "4dee35bc7e5a7fbc842a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sars2/precache-manifest.1ae87fddeb6d1e94bf2d1d869eef1270.js"
+  "/sars2/precache-manifest.69346ec732f582c94d4bd6bcdbf14997.js"
 );
 
 self.addEventListener('message', (event) => {
